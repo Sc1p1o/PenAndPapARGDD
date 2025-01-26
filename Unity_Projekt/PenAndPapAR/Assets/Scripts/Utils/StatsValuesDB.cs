ce21dcdd00ae883c6d8007571e137ba3e4e3a184
mmiller <martinberuf@gmail.com>
2025-01-26 15:59:33 +0100

    added UtilScript for CharacterTraits

---
+++ Unity_Projekt/PenAndPapAR/Assets/Scripts/Utils/StatsValuesDB.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public static class StatsValuesDB
+    {
+        private static int _strenghAttribute;
+        private static int _dexterityAttribute;
+        private static int _intelligenceAttribute;
+        private static int _constitutionAttribute;
+        private static int _wisdomAttribute;
+        private static int _charismaAttribute;
+
+        private static int _proficiencyBonus;
+
+        private static int _speed;
+
+        private static int _healthpointsMax;
+        private static int _healthpointsTemporary;
+        private static int _healthpointsCurrent;
+
+        private static int _baseAC;
+
+        private static int _baseLevel;
+
+        private static string _characerName;
+        private static string _characterRace;
+        private static string _characterClass;
+        private static string _characterGender;
+        private static string _characterSubClass;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        
+
+        // Update is called once per frame
+        static void UpdateValues()
+        {
+
+        }
+    }
+}
