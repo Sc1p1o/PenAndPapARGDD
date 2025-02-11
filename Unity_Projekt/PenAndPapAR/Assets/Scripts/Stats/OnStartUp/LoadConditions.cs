0a9c0b2a76a92f7b0180bda7ff7e738d28b82d3b
Martin <martinberuf@gmail.com>
2025-02-11 01:25:32 +0100

    Worked on Loading Conditions from DB

---
+++ Unity_Projekt/PenAndPapAR/Assets/Scripts/Stats/OnStartUp/LoadConditions.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Utils;
+
+namespace Stats.OnStartUp
+{
+    public class LoadConditions : MonoBehaviour
+    {
+        public GameObject conditionLayout;
+        public GameObject conditionPrefab;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+            LoadConditionsFromDB();
+        }
+        
+        void OnEnable()
+        {
+            DBConnector.OnStatsUpdated += LoadConditionsFromDB;
+        }
+
+        void OnDisable()
+        {
+            DBConnector.OnStatsUpdated -= LoadConditionsFromDB;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void LoadConditionsFromDB()
+        {
+            
+        }
+    }
+}
