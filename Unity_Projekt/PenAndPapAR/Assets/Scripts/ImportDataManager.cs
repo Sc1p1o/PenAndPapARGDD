5c07676d9d0f7e65430ffd856e4a45ba2675238e
SignoreNinja <jannis18.jk@gmail.com>
2025-03-11 00:28:00 +0100

    buttons have now Funktions

--- Unity_Projekt/PenAndPapAR/Assets/Scripts/ImportDataManager.cs
+++ Unity_Projekt/PenAndPapAR/Assets/Scripts/ImportDataManager.cs
@@ -3,17 +3,17 @@ using UnityEngine.UI;
 
 public class ImportDataManager : MonoBehaviour
 {
-    public GameObject characterSheetPanel; // Referenz zum Panel
-    private InputField inputField; // Private Referenz zum InputField
+    // Referenz zum Panel
+    public GameObject characterSheetPanel;
+
+    // Standard-Link
+    private string defaultLink = "https://www.dndbeyond.com/characters/137425320";
+
+    // Key für PlayerPrefs (zum Speichern des Links)
+    private const string LastSheetLinkKey = "LastSheetLink";
 
     void Start()
     {
-        // Finde das InputField im Panel
-        if (characterSheetPanel != null)
-        {
-            inputField = characterSheetPanel.GetComponentInChildren<InputField>();
-        }
-
         // Deaktiviere das Panel beim Start
         if (characterSheetPanel != null)
         {
@@ -21,19 +21,54 @@ public class ImportDataManager : MonoBehaviour
         }
     }
 
+    // Methode, um das Panel zu öffnen
     public void OnOpenCharacterSheetButtonClick()
     {
         if (characterSheetPanel != null)
         {
             // Aktiviere das Panel
             characterSheetPanel.SetActive(true);
+        }
+    }
 
-            // Setze den Fokus auf das InputField
-            if (inputField != null)
-            {
-                inputField.Select();
-                inputField.ActivateInputField();
-            }
+    // Methode für den Cancel Button (Panel schließen)
+    public void OnCancelButtonClick()
+    {
+        Debug.Log("Cancel Button wurde gedrückt");
+        if (characterSheetPanel != null)
+        {
+            // Deaktiviere das Panel
+            characterSheetPanel.SetActive(false);
+        }
+    }
+
+    // Methode für den Add Button (ohne Funktion)
+    public void OnAddButtonClick()
+    {
+        Debug.Log("Add Button wurde gedrückt, aber keine Funktion zugewiesen");
+    }
+
+    // Methode für den Load Button (letzten oder Standard-Link laden)
+    public void OnLoadButtonClick()
+    {
+        // Lade den zuletzt gespeicherten Link
+        string lastSheetLink = PlayerPrefs.GetString(LastSheetLinkKey, defaultLink);
+
+        Debug.Log("Lade Sheet-Link: " + lastSheetLink);
+
+        // Hier kannst du den Link weiterverarbeiten (z. B. in einem Browser öffnen)
+        // Beispiel: Application.OpenURL(lastSheetLink);
+    }
+
+    // Methode, um den aktuellen Link zu speichern
+    public void SaveSheetLink(string link)
+    {
+        if (!string.IsNullOrEmpty(link))
+        {
+            // Speichere den Link in PlayerPrefs
+            PlayerPrefs.SetString(LastSheetLinkKey, link);
+            PlayerPrefs.Save(); // Speichern der Änderungen
+            Debug.Log("Sheet-Link gespeichert: " + link);
         }
     }
 }
